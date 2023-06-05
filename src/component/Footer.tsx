b972ce4b258bc0d2a70577e8e98bd41bcf090d87
Charles026 <charles0260@outlook.com>
2023-06-06 01:24:57 +0800

    update footer

--- src/component/Footer.tsx
+++ src/component/Footer.tsx
@@ -1,11 +1,28 @@
-import Link from "next/link";
+
 import Image from "next/image";
 import CatHead from '@/images/cat-head.svg';
+import styles from '@/component/Footer.module.css';
 
 const Footer = () => {
     return (
-        <footer className="flex flex-col items-center justify-center w-full h-24 border-t">
-            <CatHead />
+        <footer className={`${styles.footerWrapper}  w-full mt-16 flex flex-row items-center`}>
+
+            <div className=" container flex flex-row
+        px-5 mx-auto border-t h-32 ">
+
+            <Image src={CatHead} sizes="36" alt="Ahdai">
+            </Image>
+
+            <div className="flex flex-col">
+            <p>
+                Built with ❤️ for Ahdai
+            </p>
+            <p className="text-xs text-gray-500">
+                © 2023 Charles
+            </p>
+            </div>
+
+            </div>
     </footer>  
     )
 }
